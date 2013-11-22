a7db3e000dfca8b7babcafc7f546e403aae2d6b9
Kalle Launiala <kalle.launiala@protonit.net>
2013-11-22 16:11:22 +0200

    Added initial implementation + hardcoded tests for PDF => Text => SQL Server Import

---
+++ PDFImport/PDFImport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using java.io;
+using org.apache.pdfbox.pdmodel;
+using org.apache.pdfbox.util;
+
+namespace TheBall.Import
+{
+    public class PDFImport
+    {
+        public static string GetPDFAsText(byte[] pdfData)
+        {
+            InputStream inputStream = new ByteArrayInputStream(pdfData);
+            PDDocument doc = PDDocument.load(inputStream);
+            PDFTextStripper stripper = new PDFTextStripper();
+            return stripper.getText(doc);
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheBall.Import;
+
+namespace TestConsole
+{
+    class Program
+    {
+        // Initial tests for Document ranking scores when loading PDF texts to SQL Server 2014...
+        // -- SELECT * FROM semantickeyphrasetable(FTSTest, *) ORDER BY score DESC 
+        /*
+        SELECT keyphrase, score 
+        FROM semanticsimilaritydetailstable( 
+         FTSTest, 
+         FTSContent, 4, 
+         FTSContent, 5);
+         */
+         /*
+        SELECT ID, Filename, keyphrase, score FROM semantickeyphrasetable(FTSTest, *) 
+
+        INNER JOIN FTSTest ON ID = document_key
+
+        WHERE ID = 4 or ID = 5 ORDER BY ID, score DESC
+        */
+        /*
+        SELECT ID, Filename, Score 
+
+        FROM semanticsimilaritytable(FTSTest, *, 4) 
+
+        INNER JOIN FTSTest ON ID = matched_document_key
+
+        ORDER BY score DESC
+        */
+
+
+        static void Main(string[] args)
+        {
+
+            string connStr = File.ReadAllText(@"c:\tmp\PDFsToImport\testconnstr.txt");
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("delete from FTSTest", conn);
+            cmd.ExecuteNonQuery();
+
+            string directoryToLookFor = @"c:\tmp\PDFsToImport\";
+            string[] allPDFs = Directory.GetFiles(directoryToLookFor, "*.pdf");
+            foreach (string fullName in allPDFs)
+            {
+                string fileName = Path.GetFileName(fullName);
+                byte[] fileData = File.ReadAllBytes(fullName);
+                var result = PDFImport.GetPDFAsText(fileData);
+                SqlCommand insertPDF = new SqlCommand("insert into FTSTest(Filename, FTSContent) values(@Filename, @FTSContent)", conn);
+                insertPDF.Parameters.Add("@Filename", fileName);
+                insertPDF.Parameters.Add("@FTSContent", result);
+                insertPDF.ExecuteNonQuery();
+            }
+            conn.Close();
+
+        }
+    }
+}
