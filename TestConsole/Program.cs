8e8f9273213de8b1013807273ad2af9518dbb275
Kalle Launiala <kalle.launiala@protonit.net>
2013-11-23 21:18:31 +0200

    Latest changes to section splitting

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TheBall.Import;
 
@@ -37,30 +38,101 @@ namespace TestConsole
         ORDER BY score DESC
         */
 
+        class DBFieldTextData
+        {
+            public string FieldName;
+            public string TextContent;
+        }
 
         static void Main(string[] args)
         {
 
             string connStr = File.ReadAllText(@"c:\tmp\PDFsToImport\testconnstr.txt");
+            string regexStr = File.ReadAllText(@"c:\tmp\PDFsToImport\sectionsplit_regexps.txt");
+            Regex regex = new Regex(regexStr, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase | RegexOptions.Multiline);
             SqlConnection conn = new SqlConnection(connStr);
             conn.Open();
-            SqlCommand cmd = new SqlCommand("delete from FTSTest", conn);
-            cmd.ExecuteNonQuery();
+            //SqlCommand cmd = new SqlCommand("delete from FTSTest", conn);
+            //cmd.ExecuteNonQuery();
 
             string directoryToLookFor = @"c:\tmp\PDFsToImport\";
             string[] allPDFs = Directory.GetFiles(directoryToLookFor, "*.pdf");
             foreach (string fullName in allPDFs)
             {
                 string fileName = Path.GetFileName(fullName);
+                SqlCommand queryExisting = new SqlCommand("select count(*) from FTSTest where Filename = @Filename", conn);
+                queryExisting.Parameters.Add("@Filename", fileName);
+                var existingCount = (int)queryExisting.ExecuteScalar();
+                if (existingCount > 0)
+                    continue;
+                Console.WriteLine("Converting: " + fileName);
                 byte[] fileData = File.ReadAllBytes(fullName);
                 var result = PDFImport.GetPDFAsText(fileData);
-                SqlCommand insertPDF = new SqlCommand("insert into FTSTest(Filename, FTSContent) values(@Filename, @FTSContent)", conn);
+                var matches = regex.Matches(result);
+                List<Match> matchList = new List<Match>();
+                foreach (Match match in matches)
+                {
+                    matchList.Add(match);
+                }
+                DBFieldTextData[] fieldSplits = GetSections(matchList, regex, result);
+                int[] locations = matchList.Select(m => m.Index).ToArray();
+                string txtName = fullName.Replace(".pdf", ".txt");
+                File.WriteAllText(txtName, result);
+                Console.WriteLine("Uploading: " + fileName);
+                SqlCommand insertPDF = new SqlCommand("insert into FTSTest(Filename, FTSContent) OUTPUT INSERTED.ID values(@Filename, @FTSContent)", conn);
                 insertPDF.Parameters.Add("@Filename", fileName);
                 insertPDF.Parameters.Add("@FTSContent", result);
-                insertPDF.ExecuteNonQuery();
+                //insertPDF.ExecuteNonQuery();
+                long lastID = (long) insertPDF.ExecuteScalar();
+                foreach (var fieldSplit in fieldSplits)
+                {
+                    SqlCommand updateCmd = new SqlCommand(String.Format("Update FTSTest set {0} = @content where id = @ID", fieldSplit.FieldName), conn);
+                    updateCmd.Parameters.Add("@ID", lastID);
+                    updateCmd.Parameters.Add("@content", fieldSplit.TextContent);
+                    updateCmd.ExecuteNonQuery();
+                }
+
             }
             conn.Close();
 
         }
+
+        private static DBFieldTextData[] GetSections(List<Match> matchList, Regex regex, string fullContent)
+        {
+            int contentMinimumLength = 500;
+            List<DBFieldTextData> result = new List<DBFieldTextData>();
+            if(matchList.Count != 5 && matchList.Count != 10)
+                return new DBFieldTextData[0];
+            var groupNames = regex.GetGroupNames().Skip(1).ToArray();
+            for (int i = 0; i < matchList.Count - 1; i++)
+            {
+                Match currMatch = matchList[i];
+                string currGroupName = null;
+                foreach (var candGroupName in groupNames)
+                {
+                    var candidateGroup = currMatch.Groups[candGroupName];
+                    if (candidateGroup != null && candidateGroup.Success)
+                    {
+                        currGroupName = candGroupName;
+                        break;
+                    }
+                        
+                }
+                if (currGroupName.EndsWith("End"))
+                    continue;
+                Match nextMatch = matchList[i + 1];
+                int length = nextMatch.Index - currMatch.Index;
+                if (length < 500)
+                    continue;
+                string sectionContent = fullContent.Substring(currMatch.Index, length);
+                result.Add(new DBFieldTextData
+                    {
+                        FieldName = currGroupName,
+                        TextContent = sectionContent
+                    });
+            }
+            return result.ToArray();
+        }
     }
 }
+
